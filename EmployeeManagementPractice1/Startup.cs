b26427cec807e1baafaa0e7b4deee1c8d34dc273
hridoyhr <hridoyswe205@gmail.com>
2021-02-20 00:09:43 +0600

    testing git problem

--- EmployeeManagementPractice1/Startup.cs
+++ EmployeeManagementPractice1/Startup.cs
@@ -39,6 +39,10 @@ namespace EmployeeManagementPractice1
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
+            }
 
             app.UseStaticFiles();
             app.UseRouting();
